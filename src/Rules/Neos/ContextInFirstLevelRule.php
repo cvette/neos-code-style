36cc4a91e1af3cefd9b6855837379bfc7e4be277
Sebastian Helzle <sebastian@helzle.it>
2021-04-15 16:59:15 +0200

    FEATURE: Check for @context usage in first level

---
+++ src/Rules/Neos/ContextInFirstLevelRule.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vette\Neos\CodeStyle\Rules\Neos;
+
+use Vette\FusionParser\Token;
+use Vette\Neos\CodeStyle\Files\File;
+use Vette\Neos\CodeStyle\Rules\Rule;
+
+/**
+ * Checks whether `@context` was used on the first level of a prototype
+ */
+class ContextInFirstLevelRule extends Rule
+{
+
+    protected array $tokenTypes = [
+        Token::META_PROPERTY_KEYWORD_TYPE
+    ];
+
+    function process(int $tokenStreamIndex, File $file, int $level): void
+    {
+        if ($level !== 1) {
+            return;
+        }
+
+        $prevToken = $file->getTokenStream()->getTokenAt($tokenStreamIndex - 1);
+
+        if ($prevToken && $prevToken->getType() === Token::DOT_TYPE) {
+            return;
+        }
+
+        $keyword = $file->getTokenStream()->getTokenAt($tokenStreamIndex + 1);
+
+        if ($keyword instanceof Token && $keyword->getValue() === 'context') {
+            $file->addError('No `@context` should be used in the first level of a prototype', $keyword->getLine(),
+                $keyword->getColumn(), $this->severity);
+        }
+    }
+}
