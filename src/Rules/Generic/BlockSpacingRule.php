33e2148c64828845fd41be1067474ae8325824cc
Christian Vette <6884391+cvette@users.noreply.github.com>
2022-05-04 20:32:42 +0200

    TASK: add block spacing rule

---
+++ src/Rules/Generic/BlockSpacingRule.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vette\Neos\CodeStyle\Rules\Generic;
+
+use Vette\Neos\CodeStyle\Files\File;
+use Vette\Neos\CodeStyle\Lexer\Token;
+use Vette\Neos\CodeStyle\Rules\Rule;
+
+/**
+ * Class BlockSpacingRule
+ *
+ * @package Vette\Neos\CodeStyle\Rules\Generic
+ */
+class BlockSpacingRule extends Rule
+{
+    /**
+     * @var int[]
+     */
+    protected $tokenTypes = [
+        Token::LBRACE_TYPE
+    ];
+
+    function process(int $tokenStreamIndex, File $file, int $level): void
+    {
+        $previous = $file->getTokenStream()->getTokenAt($tokenStreamIndex - 1);
+        $current = $file->getTokenStream()->getTokenAt($tokenStreamIndex);
+
+        if ($previous !== null && ($previous->getType() !== Token::WHITESPACE_TYPE || strlen($previous->getValue()) > 1)) {
+            $error = 'Expecting exactly 1 space before opening brace';
+            $file->addError($error, $current->getLine(), $current->getColumn(), $this->severity);
+        }
+    }
+}
