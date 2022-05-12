a7c8cd35da2c79c3cf5c801308ce2f5476690322
Christian Vette <6884391+cvette@users.noreply.github.com>
2022-05-12 20:50:54 +0200

    TASK: Add color to console report

--- src/Reports/Console.php
+++ src/Reports/Console.php
@@ -45,10 +45,8 @@ class Console extends Report
 
         foreach ($file->getErrors() as $error) {
             $severity = str_pad($this->formatErrorSeverity($error->getSeverity()), 5, ' ', STR_PAD_RIGHT);
-            $lines .= str_pad((string)$error->getLineNumber(), 4, ' ', STR_PAD_LEFT) . ' | ' . $severity . ' | ' . str_pad($error->getMessage(), 103, ' ', STR_PAD_RIGHT)  . PHP_EOL;
+            $lines .= $this->colorLog(str_pad((string)$error->getLineNumber(), 4, ' ', STR_PAD_LEFT) . ' | ' . $severity . ' | ' . str_pad($error->getMessage(), 103, ' ', STR_PAD_RIGHT), $error->getSeverity()) . PHP_EOL;
         }
-
-        $lines .=  str_repeat('-', 118) . PHP_EOL;
 
         $this->fileReports[] = $lines;
     }
@@ -64,6 +62,20 @@ class Console extends Report
             case Error::SEVERITY_ERROR: return 'ERROR';
             default: return 'INFO';
         }
+    }
+
+    protected function colorLog($str, $type = Error::SEVERITY_INFO): string
+    {
+        switch ($type) {
+            case Error::SEVERITY_ERROR: //error
+                return "\033[31m$str \033[0m";
+            case Error::SEVERITY_WARNING: //warning
+                return "\033[33m$str \033[0m";
+            case Error::SEVERITY_INFO: //info
+                return "\033[36m$str \033[0m";
+        }
+
+        return $str;
     }
 
     /**
