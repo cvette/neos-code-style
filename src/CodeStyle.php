cvette/neos-code-style

commit 7ca0f14e271ac39a408c688f36feb498cf9f1346

fix: psalm erros

src/CodeStyle.php
@@ -54,23 +54,23 @@ class CodeStyle
         $processor = new Processor();
         $this->config = $processor->processConfiguration(new CodeStyleConfiguration(), $configFiles);
 
-        if (!empty($parameters->getRuleset())) {
+        if ($parameters->getRuleset() !== null && $parameters->getRuleset() !== '') {
             $this->config['defaultRuleSet'] = $parameters->getRuleset();
         }
 
-        if (!empty($parameters->getReport())) {
+        if ($parameters->getReport() !== null && $parameters->getReport() !== '') {
             $this->config['defaultReport'] = $parameters->getReport();
         }
 
-        if (!empty($parameters->getFiles())) {
-            $this->config['files'] = $parameters->getFiles();
-        }
-
-        if (!empty($parameters->getIncludes())) {
+        if ($parameters->getIncludes() !== null) {
             $this->config['includes'] = $parameters->getIncludes();
         }
 
-        if (!empty($parameters->getNeosRoot())) {
+        if ($parameters->getFiles() !== null) {
+            $this->config['files'] = $parameters->getFiles();
+        }
+
+        if ($parameters->getNeosRoot() !== null && $parameters->getNeosRoot() !== '') {
             $this->config['neosRoot'] = $parameters->getNeosRoot();
         }
     }

src/CodeStyleConfiguration.php
@@ -25,6 +25,7 @@ class CodeStyleConfiguration implements ConfigurationInterface
      *
      * @psalm-suppress PossiblyUndefinedMethod
      * @psalm-suppress PossiblyNullReference
+     * @psalm-suppress UndefinedInterfaceMethod
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {

src/Files/File.php
@@ -36,8 +36,10 @@ class File
      */
     public function __construct(string $path, ?Package $package = null)
     {
+        $realPath = realpath($path);
+
         $this->path = $path;
-        $this->realPath = realpath($path) ?: '';
+        $this->realPath = $realPath === false ? '' : $realPath;
         $this->package = $package;
     }
 

src/Files/FileCollection.php
@@ -16,6 +16,7 @@ use Vette\Neos\CodeStyle\Packages\PackageCollection;
  * Class FileCollection
  *
  * @package Vette\Neos\CodeStyle\Files
+ * @template-implements Iterator<File>
  */
 class FileCollection implements Iterator, Countable
 {

src/Filter/Filter.php
@@ -13,6 +13,7 @@ use FilesystemIterator;
  * Class Filter
  *
  * @package Vette\Neos\CodeStyle\Filter
+ * @psalm-suppress MissingTemplateParam
  */
 class Filter extends RecursiveFilterIterator
 {

src/Lexer/Lexer.php
@@ -560,6 +560,10 @@ class Lexer
             $offset += strlen($match[0]);
         }
 
+        if ($token === '') {
+            return false;
+        }
+
         if (preg_match($token, $this->code, $match, 0, $this->cursor + $offset)) {
             return true;
         }

src/Lexer/TokenStream.php
@@ -11,6 +11,7 @@ use Iterator;
  * Class TokenStream
  *
  * @package Vette\FusionParser
+ * @template-implements Iterator<Token>
  */
 class TokenStream implements Iterator, Countable
 {

src/Packages/PackageCollection.php
@@ -10,6 +10,7 @@ use Iterator;
  * Class PackageCollection
  *
  * @package Vette\Neos\CodeStyle\Packages
+ * @template-implements Iterator<Package>
  */
 class PackageCollection implements Iterator
 {

commit bd03beb028434c6110ab15077bf7ef919bb812f6

BUGFIX: Custom rules and files dont work
This fixes a regression in 7ca0f14e271ac39a408c688f36feb498cf9f1346 as files and includes are empty arrays but never null.

This overwrote any config from the yaml file therefore.

src/CodeStyle.php
@@ -62,11 +62,11 @@ class CodeStyle
             $this->config['defaultReport'] = $parameters->getReport();
         }
 
-        if ($parameters->getIncludes() !== null) {
+        if (!empty($parameters->getIncludes())) {
             $this->config['includes'] = $parameters->getIncludes();
         }
 
-        if ($parameters->getFiles() !== null) {
+        if (!empty($parameters->getFiles())) {
             $this->config['files'] = $parameters->getFiles();
         }
 